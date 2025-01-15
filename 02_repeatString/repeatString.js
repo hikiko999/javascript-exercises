23a3fc3382f779772b08d660e09db0d6ddfb7899
hikiko999 <132985874+hikiko999@users.noreply.github.com>
2025-01-15 12:14:46 -0600

    Fix repeatString using loop.
    Added loop to add input string on top of itself.
    
    Added condition to output 'ERROR' if input number is negative.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(userString, n) {
+    if (n < 0) {
+        return 'ERROR'
+    }
+    
+    let myString = "";
+    for (let i = 0; i < n; i++) {
+        myString = myString + userString;
+    }
+    return myString
 };
 
 // Do not edit below this line
