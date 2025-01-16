8114316871a2339f671c36e7c39ef973e65bcd67
hikiko999 <132985874+hikiko999@users.noreply.github.com>
2025-01-15 22:26:51 -0600

    Write leapYear using if + conditional operators
    Suggested: Write the conditions first in variables to make it cleaner.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(targetYear) {
+    if ((targetYear % 4 == 0) 
+        && (!(targetYear % 100 == 0)||(targetYear % 400 == 0))){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
