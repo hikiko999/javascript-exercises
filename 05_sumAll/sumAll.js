3234c4972cbe593bd0de9b12eb47e5390192737a
hikiko999 <132985874+hikiko999@users.noreply.github.com>
2025-01-15 21:56:09 -0600

    (Test 1-2) Write sumAll using forLoop
    Partial completion of sumAll. Iterating using lower and upper limits.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(lowerLimit,upperLimit) {
+    let sum = 0
+    for (let i=lowerLimit; i <= upperLimit; i++) {
+        console.log(i)
+        sum += i;
+    };
+    return sum;
 };
 
+// console.log(sumAll(1,4));
 // Do not edit below this line
 module.exports = sumAll;
