0545c8838f3449c87ed47e13243f7b9c8b06c9f3
hikiko999 <132985874+hikiko999@users.noreply.github.com>
2025-01-15 22:52:22 -0600

    Write tempConversion using standard formulas
    Based the solutions on formulas for conversion.
    
    Used Math.round to round off to 1 decimal place.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  // Formula: (32°F − 32) × 5/9 = 0°C
+  const celsius = (fahrenheit - 32) * 5/9;
+  return Math.round(celsius*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  // Formula: (0°C × 9/5) + 32 = 32°F
+  const fahrenheit = (celsius * 9/5) + 32
+  return Math.round(fahrenheit*10)/10;
 };
 
 // Do not edit below this line
