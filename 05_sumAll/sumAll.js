40fd91861df5a3772367c8f839c0718df15e594c
hikiko999 <132985874+hikiko999@users.noreply.github.com>
2025-01-15 22:17:51 -0600

    Finish sumAll with checking for correct parameters
    Added if conditions to check which number is lower or upper limit.
    
    Added check if number is below zero (negative).
    
    Added isInteger check for parameters.
    
    Added isNan check (can be excluded because isInteger assumes number).

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,24 @@
-const sumAll = function(lowerLimit,upperLimit) {
+const sumAll = function(num1,num2) {
+    if ((num1 < 0 || num2 < 0) 
+        || (!Number.isInteger(num1)||!Number.isInteger(num2))
+        || (isNaN(num1)||isNaN(num2))) { // isNan() -> checks if not a number
+        return 'ERROR'
+    };
+
+    let lowerLimit, upperLimit;
+    if (num1 <= num2) {
+        lowerLimit = num1;
+        upperLimit = num2
+    } else {
+        lowerLimit = num2;
+        upperLimit = num1;
+    }
     let sum = 0
     for (let i=lowerLimit; i <= upperLimit; i++) {
-        console.log(i)
         sum += i;
     };
     return sum;
 };
 
-// console.log(sumAll(1,4));
 // Do not edit below this line
 module.exports = sumAll;
